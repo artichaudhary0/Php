3c8f92b4f585163631ade6bfdeb1dfcf83b57e4c
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-12-24 18:55:44 +0530

    Php form

--- form.php
+++ form.php
@@ -1,18 +1,22 @@
 <?php
-$storedPassord = $storedPassword = '$2y$10$wG8ghT6gPT9QxLgrRmp2kOtK5a5C5kdQgCgENOMz67rHc4WDFt7/y';
-echo "sbuosdfsd";
 
-// check if form is submitted?
-if($_SERVER["REQUEST_METHOD"]==="POST")
-{
+    if($_SERVER['REQUEST_METHOD']==="POST")
+    {
+        $firstName = htmlspecialchars($_POST["firstName"]);
+        $lastName = htmlspecialchars($_POST["lastName"]);
+        $dob = htmlspecialchars($_POST["dob"]);
+        $gender = htmlspecialchars($_POST["gender"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $phone = htmlspecialchars($_POST["phone"]);
+        $address = htmlspecialchars($_POST["address"]);
+        $username = htmlspecialchars($_POST["username"]);
+        $password = htmlspecialchars($_POST["password"]);
+        $firstName = htmlspecialchars($_POST["firstName"]);
+        $firstName = htmlspecialchars($_POST["firstName"]);
 
-    
-    $entered_password = $_POST['password'];
 
-    if(password_verify($entered_password,$storedPassord)){
-        echo "<p>Password is correct. Welcome</p>";
-    }else{
-        echo "<p>Password is in-correct. Please try again</p>";
+
+        echo "<p>First Name : $firstName</p>"
     }
-}
+
 ?>
